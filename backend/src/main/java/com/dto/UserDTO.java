bf4f58cfa5fb74a39a898dd86e23a6039277a776
Tanya <meronyk.tetyana@gmail.com>
2021-04-07 23:09:40 +0300

    commented extra lines

--- backend/src/main/java/com/dto/UserDTO.java
+++ backend/src/main/java/com/dto/UserDTO.java
@@ -18,17 +18,17 @@ public class UserDTO {
     private Integer id;
     private String login;
     private List<OrderDTO> orders;
-    private List<OrderDTO> salesOrders;
+//    private List<OrderDTO> salesOrders;
     public UserDTO(UserEntity user) {
         id = user.getId();
         login = user.getLogin();
         orders = new ArrayList<>();
-        salesOrders = new ArrayList<>();
+//        salesOrders = new ArrayList<>();
         for (OrderEntity o : user.getOrders()) {
             orders.add(new OrderDTO(o.getToyName(), o.getQuantity(), o.getPriority(), o.getDeadline(), o.getStatus().toString(), o.getId(), o.getSales().getId()));
         }
-        for (OrderEntity o : user.getSalesOrders()){
-            salesOrders.add(new OrderDTO(o.getToyName(), o.getQuantity(), o.getPriority(), o.getDeadline(), o.getStatus().toString(), o.getId(), o.getSales().getId()));
-        }
+//        for (OrderEntity o : user.getSalesOrders()){
+//            salesOrders.add(new OrderDTO(o.getToyName(), o.getQuantity(), o.getPriority(), o.getDeadline(), o.getStatus().toString(), o.getId(), o.getSales().getId()));
+//        }
     }
 }
